9d489a813a32dd5852bb62e926ed74e34a2bc54b
amustova <81590609+amustova@users.noreply.github.com>
2021-04-06 16:52:13 +0700

    update main.js

--- main.js
+++ main.js
@@ -10,12 +10,12 @@
  *  HIDE NAVBAR ONCLICK EVERYWHERE  *
  *                                  *
  ************************************/
-var body = document.querySelector('body');
-var nbCollapse = document.querySelector('.navbar-collapse');
+var body = document.querySelector("body");
+var nbCollapse = document.querySelector(".navbar-collapse");
 var hide = new bootstrap.Collapse(nbCollapse, { toggle: false });
 
-body.addEventListener('click', function () {
-  if (nbCollapse.classList.contains('show')) {
+body.addEventListener("click", function () {
+  if (nbCollapse.classList.contains("show")) {
     hide.toggle();
   }
 });
@@ -25,22 +25,22 @@ body.addEventListener('click', function () {
  *  HIDE/COLLAPSE NAVBAR ON SCROLL  *
  *                                  *
  ************************************/
-var navbar = document.querySelector('#navigation');
+var navbar = document.querySelector("#navigation");
 var nbHeight = parseFloat(getComputedStyle(navbar, null).height);
 var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    navbar.classList.add('scroll-down');
-    navbar.classList.remove('scroll-up');
-    if (nbCollapse.classList.contains('show')) {
+    navbar.classList.add("scroll-down");
+    navbar.classList.remove("scroll-up");
+    if (nbCollapse.classList.contains("show")) {
       hide.toggle();
     }
   } else {
-    navbar.classList.remove('scroll-down');
-    navbar.classList.add('scroll-up');
-    if (nbCollapse.classList.contains('show')) {
+    navbar.classList.remove("scroll-down");
+    navbar.classList.add("scroll-up");
+    if (nbCollapse.classList.contains("show")) {
       hide.toggle();
     }
   }
@@ -52,12 +52,12 @@ window.onscroll = function () {
  *  CAROUSEL INDICATOR add active   *
  *                                  *
  ************************************/
-var myCarousel = document.querySelector('#carouselPage');
+var myCarousel = document.querySelector("#carouselPage");
 
-myCarousel.addEventListener('slide.bs.carousel', (event) => {
-  var elementChildrens = document.querySelector('.carousel-controls').children;
-  elementChildrens.item(event.from).classList.remove('active');
-  elementChildrens.item(event.to).classList.add('active');
+myCarousel.addEventListener("slide.bs.carousel", (event) => {
+  var elementChildrens = document.querySelector(".carousel-controls").children;
+  elementChildrens.item(event.from).classList.remove("active");
+  elementChildrens.item(event.to).classList.add("active");
 });
 
 /************************************
@@ -65,7 +65,7 @@ myCarousel.addEventListener('slide.bs.carousel', (event) => {
  *  BOOTSTRAP TOOLTIP TRIGGER       *
  *                                  *
  ************************************/
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle='tooltip']'));
+var tooltipTriggerList = [].slice.call(document.querySelectorAll("[data-bs-toggle="tooltip"]"));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
 });
@@ -75,37 +75,37 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
  *  CONTACT FORM                    *
  *                                  *
  ************************************/
-var form = document.getElementById('fs-frm');
+var form = document.getElementById("fs-frm");
 
 async function handleSubmit(event) {
   event.preventDefault();
-  var status = document.querySelector('.toast-body');
+  var status = document.querySelector(".toast-body");
   var data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   })
     .then((response) => {
-      status.innerHTML = 'Thanks for your submission!';
+      status.innerHTML = "Thanks for your submission!";
       toastList.forEach((toast) => toast.show());
       form.reset();
     })
     .catch((error) => {
-      status.innerHTML = 'Oops! There was a problem submitting your form';
+      status.innerHTML = "Oops! There was a problem submitting your form";
       toastList.forEach((toast) => toast.show());
     });
 }
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
 
 /************************************
  *                                  *
  *  TOAST FORM                      *
  *                                  *
  ************************************/
-var toastElList = [].slice.call(document.querySelectorAll('.toast'));
+var toastElList = [].slice.call(document.querySelectorAll(".toast"));
 var toastList = toastElList.map(function (toastEl) {
   return new bootstrap.Toast(toastEl);
 });
@@ -114,7 +114,7 @@ var toastList = toastElList.map(function (toastEl) {
 --------------------------------------
 */
 /*
-document.getElementById('toastbtn').onclick = function() {
+document.getElementById("toastbtn").onclick = function() {
 	toastList.forEach(toast => toast.show());
     }
 */
