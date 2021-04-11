5b32f39f7c78c71876fa54e6fbd96cbd808b39e4
amustova <81590609+amustova@users.noreply.github.com>
2021-04-11 19:08:05 +0700

    update main.js

--- main.js
+++ main.js
@@ -136,8 +136,11 @@
  *  ANIMATE ON SCROLL               *
  *                                  *
  ************************************/
-AOS.init({
-  duration: 1000,
-  easing: 'ease-in-out',
-  once: true,
-});
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
